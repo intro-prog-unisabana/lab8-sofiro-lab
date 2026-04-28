fe3dda96972c62c178ad41bdf7f579325ad91161
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-27 22:47:57 -0500

    kok

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
 # TODO: Implementar CLI según README.md
+import sys
+from todo_manager import read_todo_file, write_todo_file
+
+try:
+    if len(sys.argv) < 2:
+        raise IndexError("Insufficient arguments provided!")
+
+    file_path = sys.argv[1]
+
+    if len(sys.argv) < 3:
+        sys.exit()
+
+    comando = sys.argv[2]
+
+    tareas = read_todo_file(file_path)
+
+    if comando == "view":
+        print("Tasks:")
+        for tarea in tareas:
+            print(tarea)
+
+    elif comando == "add":
+        if len(sys.argv) < 4:
+            raise IndexError('Task description required for "add".')
+
+     
