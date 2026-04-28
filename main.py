cbfc7c4a761b6910ecedf1d8e2f8ca017de6d483
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-27 22:51:33 -0500

    bn

--- main.py
+++ main.py
@@ -8,45 +8,66 @@ try:
     if len(sys.argv) < 2:
         raise IndexError("Insufficient arguments provided!")
 
-    file_path = sys.argv[1]
+    if sys.argv[1] == "--help":
+        print("""Usage: python main.py <file_path> <command> [arguments]...
 
-    if len(sys.argv) < 3:
+Commands:
+  add "task"    - Add a task to the list.
+  remove "task" - Remove a task from the list.
+  view          - Display all tasks.
+
+Examples:
+  python main.py tasks.txt add "Buy groceries"
+  python main.py tasks.txt remove "Do laundry"
+  python main.py tasks.txt view
+  python main.py tasks.txt add "Call mom" remove "Take out trash" view""")
         sys.exit()
 
-    comando = sys.argv[2]
+    file_path = sys.argv[1]
+
+    if len(sys.argv) == 2:
+        sys.exit()
 
     tareas = read_todo_file(file_path)
+    i = 2  
 
-    if comando == "view":
-        print("Tasks:")
-        for tarea in tareas:
-            print(tarea)
+    while i < len(sys.argv):
+        comando = sys.argv[i]
 
-    elif comando == "add":
-        if len(sys.argv) < 4:
-            raise IndexError('Task description required for "add".')
-        
-        nueva_tarea = sys.argv[3]
-        tareas.append(nueva_tarea)
-        write_todo_file(file_path, tareas)
-        print(f'Task "{nueva_tarea}" added.')
+        if comando == "view":
+            print("Tasks:")
+            for tarea in tareas:
+                print(tarea)
+            i += 1
 
-    elif comando == "remove":
-        if len(sys.argv) < 4:
-            raise IndexError('Task description required for "remove".')
+        elif comando == "add":
+            if i + 1 >= len(sys.argv):
+                raise IndexError('Task description required for "add".')
 
-        tarea_eliminar = sys.argv[3]
+            nueva_tarea = sys.argv[i + 1]
+            tareas.append(nueva_tarea)
+            print(f'Task "{nueva_tarea}" added.')
+            i += 2
 
-        try:
-            tareas.remove(tarea_eliminar)
-            write_todo_file(file_path, tareas)
-            print(f'Task "{tarea_eliminar}" removed.')
-        except ValueError:
-            print(f'Task "{tarea_eliminar}" not found.')
+        elif comando == "remove":
+            if i + 1 >= len(sys.argv):
+                raise IndexError('Task description required for "remove".')
 
-    else:
-        raise ValueError("Command not found!")
-    
+            tarea_eliminar = sys.argv[i + 1]
+
+            try:
+                tareas.remove(tarea_eliminar)
+                print(f'Task "{tarea_eliminar}" removed.')
+            except ValueError:
+                print(f'Task "{tarea_eliminar}" not found.')
+
+            i += 2
+
+        else:
+            raise ValueError("Command not found!")
+
+    write_todo_file(file_path, tareas)
+
 except IndexError as e:
     print(e)
 
