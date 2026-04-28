a50f1f519756ef8d00dbe74e6afc597e48d93d24
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-27 22:48:24 -0500

    lp

--- main.py
+++ main.py
@@ -25,5 +25,30 @@ try:
     elif comando == "add":
         if len(sys.argv) < 4:
             raise IndexError('Task description required for "add".')
+        
+        nueva_tarea = sys.argv[3]
+        tareas.append(nueva_tarea)
+        write_todo_file(file_path, tareas)
+        print(f'Task "{nueva_tarea}" added.')
 
-     
+    elif comando == "remove":
+        if len(sys.argv) < 4:
+            raise IndexError('Task description required for "remove".')
+
+        tarea_eliminar = sys.argv[3]
+
+        try:
+            tareas.remove(tarea_eliminar)
+            write_todo_file(file_path, tareas)
+            print(f'Task "{tarea_eliminar}" removed.')
+        except ValueError:
+            print(f'Task "{tarea_eliminar}" not found.')
+
+    else:
+        raise ValueError("Command not found!")
+    
+except IndexError as e:
+    print(e)
+
+except ValueError as e:
+    print(e)
